7d08e0fe91568c7009c707509506585019c70c83
John Box <johnboxwon@gmail.com>
2024-03-05 19:11:11 +0200

    add test for SCHEME://HOST:PORT/INDEX address format

--- actix-redis/tests/test_redis.rs
+++ actix-redis/tests/test_redis.rs
@@ -75,3 +75,14 @@ fn parse_host_port_index_redis_address() {
     assert_eq!(password, None);
     assert_eq!(index, Some("5".to_string()));
 }
+
+#[test]
+fn parse_scheme_host_port_index_redis_address() {
+    let addr = "redis://127.0.0.1:6378/5";
+
+    let (addr, password, index) = RedisActor::parse_url(addr.into());
+
+    assert_eq!(addr, "127.0.0.1:6378".to_string());
+    assert_eq!(password, None);
+    assert_eq!(index, Some("5".to_string()));
+}
