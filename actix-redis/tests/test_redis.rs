af6cb2e9463c299b6e7dad3b4c6bcb75de4ce49b
John Box <johnboxwon@gmail.com>
2024-03-05 19:02:07 +0200

    add test for SCHEME://HOST:PORT address format

--- actix-redis/tests/test_redis.rs
+++ actix-redis/tests/test_redis.rs
@@ -5,7 +5,7 @@ use actix_redis::{Command, Error, RedisActor, RespValue};
 
 #[actix_web::test]
 async fn test_error_connect() {
-    let addr = RedisActor::start("localhost:54000");
+    let addr = RedisActor::start("127.0.0.1:54000");
     let _addr2 = addr.clone();
 
     let res = addr.send(Command(resp_array!["GET", "test"])).await;
@@ -43,7 +43,7 @@ async fn test_redis() {
 
 
 #[test]
-fn parse_naked_redis_address() {
+fn parse_host_port_redis_address() {
     let addr = "127.0.0.1:6378";
 
     let (addr, password, index) = RedisActor::parse_url(addr.into());
@@ -51,66 +51,16 @@ fn parse_naked_redis_address() {
     assert_eq!(addr, "127.0.0.1:6378".to_string());
     assert_eq!(password, None);
     assert_eq!(index, None);
-
-
-
 }
-#[test]
-fn parse_redis_address_with_index() {
-    let address = "redis://127.0.0.1:6378/5";
 
-    let (host_port, password, index) = RedisActor::parse_url(address.to_string());
-    assert_eq!(host_port, "127.0.0.1:6378".to_string());
+
+#[test]
+fn parse_scheme_host_port_redis_address() {
+    let addr = "redis://127.0.0.1:6378";
+
+    let (addr, password, index) = RedisActor::parse_url(addr.into());
+
+    assert_eq!(addr, "127.0.0.1:6378".to_string());
     assert_eq!(password, None);
-    assert_eq!(index, Some("5".to_string()));
-
-    let (host_port, password, index) =
-        RedisActor::parse_url(address.trim_start_matches("redis://").to_string());
-    assert_eq!(host_port, "127.0.0.1:6378".to_string());
-    assert_eq!(password, None);
-    assert_eq!(index, Some("5".to_string()));
-}
-
-#[test]
-fn parse_redis_address_with_password() {
-    let address = "redis://:68831eec-e7dc-4ac3-9292-b5e2107bb46d@127.0.0.1:6378";
-
-    let (host_port, password, index) = RedisActor::parse_url(address.to_string());
-    assert_eq!(host_port, "127.0.0.1:6378".to_string());
-    assert_eq!(
-        password,
-        Some("68831eec-e7dc-4ac3-9292-b5e2107bb46d".to_string())
-    );
     assert_eq!(index, None);
-
-    let (host_port, password, index) =
-        RedisActor::parse_url(address.trim_start_matches("redis://").to_string());
-    assert_eq!(host_port, "127.0.0.1:6378".to_string());
-    assert_eq!(
-        password,
-        Some("68831eec-e7dc-4ac3-9292-b5e2107bb46d".to_string())
-    );
-    assert_eq!(index, None);
-}
-
-#[test]
-fn parse_redis_address_with_password_and_index() {
-    let address = "redis://:68831eec-e7dc-4ac3-9292-b5e2107bb46d@127.0.0.1:6378/5";
-
-    let (host_port, password, index) = RedisActor::parse_url(address.to_string());
-    assert_eq!(host_port, "127.0.0.1:6378".to_string());
-    assert_eq!(
-        password,
-        Some("68831eec-e7dc-4ac3-9292-b5e2107bb46d".to_string())
-    );
-    assert_eq!(index, Some("5".to_string()));
-
-    let (host_port, password, index) =
-        RedisActor::parse_url(address.trim_start_matches("redis://").to_string());
-    assert_eq!(host_port, "127.0.0.1:6378".to_string());
-    assert_eq!(
-        password,
-        Some("68831eec-e7dc-4ac3-9292-b5e2107bb46d".to_string())
-    );
-    assert_eq!(index, Some("5".to_string()));
 }
