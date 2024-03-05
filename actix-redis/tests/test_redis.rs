e9864397a1d2c1ad532eab85cde60a479628aed8
John Box <johnboxwon@gmail.com>
2024-03-05 19:17:33 +0200

    add test for PASSWORD@HOST:PORT/INDEX address format

--- actix-redis/tests/test_redis.rs
+++ actix-redis/tests/test_redis.rs
@@ -105,3 +105,21 @@ fn parse_password_host_port_redis_address() {
     assert_eq!(password, Some("password123".to_string()));
     assert_eq!(index, None);
 }
+#[test]
+fn parse_password_host_port_index_redis_address() {
+    let addr = "password123@127.0.0.1:6378/5";
+
+    let (addr, password, index) = RedisActor::parse_url(addr.into());
+
+    assert_eq!(addr, "127.0.0.1:6378".to_string());
+    assert_eq!(password, Some("password123".to_string()));
+    assert_eq!(index, Some("5".to_string()));
+
+    let addr = ":password123@127.0.0.1:6378/5";
+
+    let (addr, password, index) = RedisActor::parse_url(addr.into());
+
+    assert_eq!(addr, "127.0.0.1:6378".to_string());
+    assert_eq!(password, Some("password123".to_string()));
+    assert_eq!(index, Some("5".to_string()));
+}
